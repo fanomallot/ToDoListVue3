47a402313b4085a6e6c0b8b63c2619845a26c42a
fanomallot <aafano19@gmail.com>
2022-07-18 15:57:08 +0300

    save local data

--- scripts.js
+++ scripts.js
@@ -8,21 +8,30 @@ const handlingToDo = {
   computed: {
     toDoNotExist() {
       return !this.toDo
+    },
+    saveLocalData() {
+      localStorage.setItem('todos', JSON.stringify(this.toDoList))
     }
   },
   methods: {
     save() {
       if (!this.toDoNotExist) {
-        this.toDoList.push({title: this.toDo, done: false})
-        this.toDo = null
+        this.toDoList.push({title: this.toDo, done: false});
+        this.toDo = null;
+        saveLocalData();
       }
     },
     done(index) {
-      this.toDoList[index].done = !this.toDoList[index].done
+      this.toDoList[index].done = !this.toDoList[index].done;
+      saveLocalData();
     },
     clearAll() {
-      this.toDoList = []
+      this.toDoList = [];
+      saveLocalData();
     }
+  },
+  created() {
+    this.toDoList = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : this.toDoList
   },
 };
 
